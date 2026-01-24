a727c0262279dbbb40adfc33dc75de4846cada90
Piyush Rana <145906381+piyushrana31@users.noreply.github.com>
2026-01-24 10:33:40 +0530

    Time: 0 ms (100%), Space: 23.9 MB (54.16%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,23 +3,21 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
 
-        int j=-1;
+        int i=0;
+        int j=1;
 
-        for(int i=0; i<n; i++) {
-            if(nums[i]==0) {
-                j = i;
-                break;
-            }
-        }
-
-        if(j==-1) return;
-
-        for(int i=j+1; i<n; i++) {
-            if(nums[i]!=0) {
-                swap(nums[j], nums[i]);
+        while(j<n) {
+            if(nums[i] == 0 && nums[j] != 0) {
+                swap(nums[i], nums[j]);
+                i++;
                 j++;
+            } else if(nums[i] == 0 && nums[j] == 0) {
+                j++;
+            } else {
+                i++;
+                if(i==j) j++;
             }
-        }
 
+        }
     }
 };
