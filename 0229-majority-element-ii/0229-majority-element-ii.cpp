70a65feba5cd34daaa757c1ee27ec29fa11b7db3
Piyush Rana <145906381+piyushrana31@users.noreply.github.com>
2026-01-28 16:01:39 +0530

    Time: 0 ms (100%), Space: 23.6 MB (63.18%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,22 +2,21 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-
-        int el1 = nums[0], el2 = nums[0];
-        int count1 = 0, count2 = 0;
-
         vector<int> ans;
 
+        int ele1 = nums[0]; int ele2 = nums[0];
+        int count1 = 0; int count2 = 0;
+
         for(int i=0; i<n; i++) {
-            if(count1==0 && nums[i] != el2) {
+            if(count1 == 0 && nums[i] != ele2) {
                 count1 = 1;
-                el1 = nums[i];
-            } else if(count2 == 0 && nums[i] != el1) {
+                ele1 = nums[i];
+            } else if(count2 == 0 && nums[i] != ele1) {
                 count2 = 1;
-                el2 = nums[i];
-            } else if(el1 == nums[i]) {
+                ele2 = nums[i];
+            } else if(ele1 == nums[i]) {
                 count1++;
-            } else if(el2 == nums[i]) {
+            } else if(ele2 == nums[i]) {
                 count2++;
             } else {
                 count1--;
@@ -25,14 +24,14 @@ public:
             }
         }
 
-        count1 = 0, count2 = 0;
+        count1 = 0; count2 = 0;
         for(int i=0; i<n; i++) {
-            if(nums[i] == el1) count1++;
-            else if(nums[i] == el2) count2++;
+            if(ele1 == nums[i]) count1++;
+            else if(ele2 == nums[i]) count2++;
         }
 
-        if(count1 > n/3) ans.push_back(el1);
-        if(count2 > n/3) ans.push_back(el2);
+        if(count1 > n/3) ans.push_back(ele1);
+        if(count2 > n/3) ans.push_back(ele2);
 
         return ans;
 
