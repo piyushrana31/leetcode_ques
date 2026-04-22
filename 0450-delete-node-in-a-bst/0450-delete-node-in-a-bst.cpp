ebb62c6338700de8ec4fe0879de7c83d1179e688
Piyush Rana <145906381+piyushrana31@users.noreply.github.com>
2026-04-22 19:39:04 +0530

    Time: 0 ms (100%), Space: 34.3 MB (49.87%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root == nullptr) return nullptr;
+
+        if(root->val == key) {
+            return helper(root);
+        }
+
+        TreeNode* curr = root;
+
+        while(curr) {
+            if(key < curr->val) {
+                if(curr->left && curr->left->val == key) {
+                    curr->left = helper(curr->left);
+                    break;
+                }else {
+                    curr = curr->left;
+                }
+            }else {
+                if(curr->right && curr->right->val == key) {
+                    curr->right = helper(curr->right);
+                    break;
+                }else {
+                    curr = curr->right;
+                }
+            }
+        }
+
+        return root;
+    }
+
+    TreeNode* helper(TreeNode* root) {
+        if(root->left == nullptr) return root->right;
+        else if(root->right == nullptr) return root->left;
+
+        TreeNode* rightNode = root->right;
+        TreeNode* lastRight = findLastRight(root->left);
+        lastRight->right = rightNode;
+        
+        return root->left;
+    }
+
+    TreeNode* findLastRight(TreeNode* root) {
+        if(root == nullptr) return nullptr;
+
+        TreeNode* curr = root;
+        while(curr->right) {
+            curr = curr->right;
+        }
+
+        return curr;
+    }
+
+};
