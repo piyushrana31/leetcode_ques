b4b7d4dae4166115458ffd5ab1e06a1f3c74db5e
Piyush Rana <145906381+piyushrana31@users.noreply.github.com>
2026-05-21 21:14:49 +0530

    Time: 0 ms (100%), Space: 26.2 MB (33.44%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -16,10 +16,10 @@ public:
 
             if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) {
                 return nums[mid];
-            }else if((mid%2==0 && nums[mid] == nums[mid+1]) || (mid%2==1 && nums[mid] == nums[mid-1])) {
-                low = mid + 1;
-            }else {
+            }else if((mid%2==0 && nums[mid] == nums[mid-1]) || (mid%2==1 && nums[mid] == nums[mid+1])) {
                 high = mid - 1;
+            }else {
+                low = mid + 1;
             }
         }
 
